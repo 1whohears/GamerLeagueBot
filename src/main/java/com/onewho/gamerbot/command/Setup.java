5c38b9d2de2ddc101a25249f1e941a13c0ff4bd9
1whohears <79810154+1whohears@users.noreply.github.com>
2022-08-26 10:26:47 -0500

    read and write league data with gson

--- src/main/java/com/onewho/gamerbot/command/Setup.java
+++ src/main/java/com/onewho/gamerbot/command/Setup.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Setup implements ICommand {
@@ -28,23 +29,30 @@ public class Setup implements ICommand {
 	public boolean runCommand(MessageReceivedEvent event, String[] params) {
 		System.out.println("running setup command");
 		Guild guild = event.getGuild();
-		List<GuildChannel> channels = guild.getChannels();
-		if (guild.getCategoriesByName("Gamer League", true).size() == 0) guild.createCategory("Gamer League").complete();
-		Category gamerCat = guild.getCategoriesByName("Gamer League", true).get(0);
-		if (!doesChannelExist("bot-commands", channels)) guild.createTextChannel("bot-commands", gamerCat).complete();
+		// TODO use guild data to get roles and channels by id
+		//setup roles
+		Role gamerRole = null;
+		if (guild.getRolesByName("Gamer League", true).size() == 0) 
+			gamerRole = guild.createRole().complete();
+		else gamerRole = null;
+		//setup category
+		Category gamerCat = null;
+		if (guild.getCategoriesByName("Gamer League", true).size() == 0) 
+			gamerCat = guild.createCategory("Gamer League").complete();
+		else gamerCat = null;
+		//setup channels
+		/*if (!doesChannelExist("bot-commands", channels)) guild.createTextChannel("bot-commands", gamerCat).complete();
 		if (!doesChannelExist("options", channels)) guild.createTextChannel("options", gamerCat).complete();
 		if (!doesChannelExist("set-history", channels)) guild.createTextChannel("set-history", gamerCat).complete();
 		if (!doesChannelExist("ranks", channels)) guild.createTextChannel("ranks", gamerCat).complete();
-		if (!doesChannelExist("pairings", channels)) guild.createTextChannel("pairings", gamerCat).complete();
-		
-		channels = guild.getChannels();
+		if (!doesChannelExist("pairings", channels)) guild.createTextChannel("pairings", gamerCat).complete();*/
 		
 		System.out.println("setup command complete");
 		event.getChannel().sendMessage("Bot Channel Setup Complete!").queue();
 		return true;
 	}
 	
-	private boolean doesChannelExist(String name, List<GuildChannel> channels) {
+	/*private boolean doesChannelExist(String name, List<GuildChannel> channels) {
 		for (GuildChannel c : channels) if (c.getName().equals(name)) return true;
 		return false;
 	}
@@ -52,6 +60,6 @@ public class Setup implements ICommand {
 	private GuildChannel getChannelByName(String name, List<GuildChannel> channels) {
 		for (GuildChannel c : channels) if (c.getName().equals(name)) return c;
 		return null;
-	}
+	}*/
 
 }
---
+++ src/main/java/com/onewho/gamerbot/data/LeagueData.java
@@ -0,0 +1,57 @@
+package com.onewho.gamerbot.data;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public class LeagueData {
+	
+	private static Gson gson = null;
+	private static JsonObject json = new JsonObject();
+	private static String dataFileName = "data.json";
+	
+	public static JsonObject readJsonData() throws IOException {
+		if (gson == null) {
+			GsonBuilder builder = new GsonBuilder();
+			builder.setPrettyPrinting();
+			gson = builder.create();
+		}
+		if (!Files.exists(Paths.get(dataFileName))) {
+			json.add("guilds", new JsonArray());
+			saveData();
+		} else {
+			Reader reader = Files.newBufferedReader(Paths.get(dataFileName));
+			json = gson.fromJson(reader, JsonObject.class);
+			reader.close();
+		}
+		return json;
+	}
+	
+	public static void saveData() throws IOException {
+		String data = gson.toJson(json);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(dataFileName));
+	    writer.write(data);
+	    writer.close();
+	}
+	
+	public static JsonObject getGuildDataById(long id) throws IOException {
+		JsonArray guilds = json.get("guilds").getAsJsonArray();
+		for (int i = 0; i < guilds.size(); ++i) 
+			if (guilds.get(i).getAsJsonObject().get("id").getAsLong() == id) 
+				return guilds.get(i).getAsJsonObject();
+		JsonObject guild = new JsonObject();
+		guild.addProperty("id", id);
+		guilds.add(guild);
+		saveData();
+		return guild;
+	}
+	
+}
