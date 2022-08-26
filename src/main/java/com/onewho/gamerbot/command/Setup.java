30761c0997f7f62af3bca8cfcd7a6dcb31f257c7
1whohears <1whohears28@gmail.com>
2022-08-26 00:02:48 -0500

    setup command creates channels

--- src/main/java/com/onewho/gamerbot/command/Setup.java
+++ src/main/java/com/onewho/gamerbot/command/Setup.java
@@ -2,6 +2,7 @@ package com.onewho.gamerbot.command;
 
 import java.util.List;
 
+import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -28,18 +29,29 @@ public class Setup implements ICommand {
 		System.out.println("running setup command");
 		Guild guild = event.getGuild();
 		List<GuildChannel> channels = guild.getChannels();
-		if (!doesChannelExist("bot-commands", channels)) guild.createTextChannel("bot-commands").queue();
-		// this experiment shows guild.getChannels() does not get updated by guild.createTextChannel(...).queue()
-		//if (!doesChannelExist("bot-commands", channels)) guild.createTextChannel("bot-commands").queue();
+		if (guild.getCategoriesByName("Gamer League", true).size() == 0) guild.createCategory("Gamer League").complete();
+		Category gamerCat = guild.getCategoriesByName("Gamer League", true).get(0);
+		if (!doesChannelExist("bot-commands", channels)) guild.createTextChannel("bot-commands", gamerCat).complete();
+		if (!doesChannelExist("options", channels)) guild.createTextChannel("options", gamerCat).complete();
+		if (!doesChannelExist("set-history", channels)) guild.createTextChannel("set-history", gamerCat).complete();
+		if (!doesChannelExist("ranks", channels)) guild.createTextChannel("ranks", gamerCat).complete();
+		if (!doesChannelExist("pairings", channels)) guild.createTextChannel("pairings", gamerCat).complete();
 		
-		event.getChannel().sendMessage("Bot Channel Setup Complete!").queue();
+		channels = guild.getChannels();
+		
 		System.out.println("setup command complete");
+		event.getChannel().sendMessage("Bot Channel Setup Complete!").queue();
 		return true;
 	}
 	
 	private boolean doesChannelExist(String name, List<GuildChannel> channels) {
 		for (GuildChannel c : channels) if (c.getName().equals(name)) return true;
 		return false;
+	}
+	
+	private GuildChannel getChannelByName(String name, List<GuildChannel> channels) {
+		for (GuildChannel c : channels) if (c.getName().equals(name)) return c;
+		return null;
 	}
 
 }
