d9887bbeda09307b460765d3eb4f577e243c529b
1whohears <1whohears28@gmail.com>
2022-08-30 00:57:21 -0500

    report admin

--- src/main/java/com/onewho/gamerbot/command/ReportAdmin.java
+++ src/main/java/com/onewho/gamerbot/command/ReportAdmin.java
@@ -10,16 +10,16 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public class Report implements ICommand {
+public class ReportAdmin implements ICommand {
 
 	@Override
 	public boolean getNeedsAdmin() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public String getCommandString() {
-		return "report";
+		return "reportadmin";
 	}
 
 	@Override
@@ -29,35 +29,44 @@ public class Report implements ICommand {
 
 	@Override
 	public boolean runCommand(MessageReceivedEvent event, String[] params) {
-		if (params.length != 5) {
-			event.getChannel().sendMessage("WRONG! do: ~report [id] [your score] [opponent score] [opponent ping]").queue();
+		if (params.length != 6) {
+			event.getChannel().sendMessage("WRONG! do: ~report [id] [p1 ping] [p1 score] [p2 ping] [p2 score]").queue();
 			return true;
 		}
 		int id = -1, s1 = -1, s2 = -1;
-		long pingId = -1;
+		long pingId1 = -1, pingId2 = -1;
+		if (checkIfMention(params[2])) {
+			event.getChannel().sendMessage("BRUH! "+params[2]+" is not a mention!").queue();
+			return true;
+		}
 		if (checkIfMention(params[4])) {
 			event.getChannel().sendMessage("BRUH! "+params[4]+" is not a mention!").queue();
 			return true;
 		}
-		String pingString = params[4].substring(2, params[4].length()-1);
+		String pingS1 = params[2].substring(2, params[2].length()-1);
+		String pingS2 = params[4].substring(2, params[4].length()-1);
 		try {
 			id = Integer.parseInt(params[1]);
-			s1 = Integer.parseInt(params[2]);
-			s2 = Integer.parseInt(params[3]);
-			pingId = Long.parseLong(pingString);
+			s1 = Integer.parseInt(params[3]);
+			s2 = Integer.parseInt(params[5]);
+			pingId1 = Long.parseLong(pingS1);
+			pingId2 = Long.parseLong(pingS2);
 		} catch (NumberFormatException e) {
 		}
 		if (id == -1) {
 			event.getChannel().sendMessage("IDIOT! "+params[1]+" is not a number!").queue();
 			return true;
 		} else if (s1 == -1) {
-			event.getChannel().sendMessage("UHHGGG! "+params[2]+" is not a number!").queue();
+			event.getChannel().sendMessage("UHHGGG! "+params[3]+" is not a number!").queue();
 			return true;
 		} else if (s2 == -1) {
-			event.getChannel().sendMessage("WHY!? "+params[3]+" is not a number!").queue();
+			event.getChannel().sendMessage("WHY!? "+params[5]+" is not a number!").queue();
 			return true;
-		} else if (pingId == -1) {
-			event.getChannel().sendMessage("DONKEYKONG? you didn't mention/ping your opponent correctly!").queue();
+		} else if (pingId1 == -1) {
+			event.getChannel().sendMessage("OUCH! you didn't mention/ping player 1 correctly!").queue();
+			return true;
+		} else if (pingId2 == -1) {
+			event.getChannel().sendMessage("YIKES! you didn't mention/ping player 2 correctly!").queue();
 			return true;
 		}
 		Guild guild = event.getGuild();
@@ -67,24 +76,12 @@ public class Report implements ICommand {
 			event.getChannel().sendMessage("INCORRECT! The set with id "+id+" does not exist!").queue();
 			return true;
 		}
-		ReportResult result = set.report(event.getAuthor().getIdLong(), pingId, s1, s2, UtilCalendar.getCurrentDateString());
-		switch (result) {
-		case IDsDontMatch:
-			event.getChannel().sendMessage("WOW! This set id does not have those players!").queue();
-			break;
-		case ScoreConflict:
-			event.getChannel().sendMessage("This conflicts with the score that your opponent reported! "
-					+ "If you are correct have your opponent report again, or get a hold of an admin.").queue();
-			break;
-		case SetVerified:
-			event.getChannel().sendMessage("Set reported and verified by opponent!").queue();
-			break;
-		case WaitingForOpponent:
-			event.getChannel().sendMessage("Set reported. Waiting for opponent to verify!").queue();
-			break;
-		case AlreadyVerified:
-			event.getChannel().sendMessage("This set has already been verified. Admin required to update.").queue();
-			break;
+		ReportResult result = set.reportAdmin(pingId1, pingId2, s1, s2, UtilCalendar.getCurrentDateString());
+		if (result == ReportResult.IDsDontMatch) {
+			event.getChannel().sendMessage("LOL! This set id does not have those players!").queue();
+			return true;
+		} else if (result == ReportResult.SetVerified) {
+			event.getChannel().sendMessage("Admin Override Successful!").queue();
 		}
 		//display new sets
 		TextChannel pairsChannel = guild.getChannelById(TextChannel.class, gdata.getChannelId("pairings"));
@@ -96,5 +93,5 @@ public class Report implements ICommand {
 	private boolean checkIfMention(String m) {
 		return m.length() > 10 && m.charAt(0) == '<' && m.charAt(1) == '@' && m.charAt(m.length()-1) == '>';
 	}
-
+	
 }
