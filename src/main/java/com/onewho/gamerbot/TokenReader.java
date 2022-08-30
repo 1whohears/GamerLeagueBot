8f30e83dc07cfeaba4d93e4f0ec51001348cd328
1whohears <1whohears28@gmail.com>
2022-08-29 22:49:11 -0500

    oops

--- src/main/java/com/onewho/gamerbot/TokenReader.java
+++ src/main/java/com/onewho/gamerbot/TokenReader.java
@@ -1,18 +1,18 @@
 package com.onewho.gamerbot;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 
 public class TokenReader {
 	
-	public static String getJDAToken() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(new File("token.txt")));
-		String st = "";
-		while (br.readLine() != null) st += br.readLine();
-		br.close();
-		return st;
+	public static String getJDAToken() throws JsonSyntaxException, JsonIOException, IOException {
+		return new GsonBuilder().create().fromJson(Files.newBufferedReader(Paths.get("token.json")), JsonObject.class).get("token").getAsString();
 	}
 	
 }
