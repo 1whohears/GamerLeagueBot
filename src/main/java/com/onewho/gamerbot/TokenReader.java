f84a595158213ffa71921a7d30523265629788e1
1whohears <1whohears28@gmail.com>
2022-08-29 22:25:19 -0500

    Update TokenReader.java

--- src/main/java/com/onewho/gamerbot/TokenReader.java
+++ src/main/java/com/onewho/gamerbot/TokenReader.java
@@ -3,7 +3,7 @@ package com.onewho.gamerbot;
 public class TokenReader {
 	
 	public static String getJDAToken() {
-		return "MTAxMjE2MTAzNTk2NjM1MzQwOA.GE6zm7.uZYAb9flBJAa5gstgoVJBmeCu8qBLtKQt7gJtk";
+		return "lol";
 	}
 	
 }
