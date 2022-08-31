33037b48d3bd3fea67c54b2e443001dae6c2f6b6
1whohears <1whohears28@gmail.com>
2022-08-30 22:04:08 -0500

    important

--- src/main/java/com/onewho/gamerbot/command/Help.java
+++ src/main/java/com/onewho/gamerbot/command/Help.java
@@ -1,5 +1,7 @@
 package com.onewho.gamerbot.command;
 
+import java.util.concurrent.TimeUnit;
+
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Help implements ICommand {
@@ -21,7 +23,11 @@ public class Help implements ICommand {
 
 	@Override
 	public boolean runCommand(MessageReceivedEvent event, String[] params) {
-		event.getChannel().sendMessage("LMAO!!!").queue();
+		event.getChannel().sendMessage("hmmmm....").queue();
+		event.getChannel().sendMessage("after some serious consideration...").queueAfter(2, TimeUnit.SECONDS);
+		event.getChannel().sendMessage("I think your problem is...").queueAfter(5, TimeUnit.SECONDS);
+		event.getChannel().sendMessage("I forgot").queueAfter(10, TimeUnit.SECONDS);
+		event.getChannel().sendMessage("sorry!").queueAfter(11, TimeUnit.SECONDS);
 		return true;
 	}
 
