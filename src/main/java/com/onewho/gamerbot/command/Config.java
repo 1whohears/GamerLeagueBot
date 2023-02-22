2cc11cc6122ea59ad5653b7954413ce90053b210
1whohears <1whohears28@gmail.com>
2023-02-21 22:22:07 -0600

    config uses sub command

--- src/main/java/com/onewho/gamerbot/command/Config.java
+++ src/main/java/com/onewho/gamerbot/command/Config.java
@@ -10,10 +10,6 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Config extends LeagueCommand {
 	
-	public Config() {
-		subCommands.put("max-sets-per-week", );
-	}
-	
 	@Override
 	public boolean getNeedsTO() {
 		return true;
@@ -35,6 +31,126 @@ public class Config extends LeagueCommand {
 				+ " Settings: `max-sets-per-week`, `weeks-before-auto-inactive`, `weeks-before-set-expires`,"
 				+ " `weeks-before-set-repeat`, `default-score`, `K` (elo K constant), `auto-gen-pairs`, `auto-update-ranks`";
 	}
+	
+	public Config() {
+		subCommands.put("max-sets-per-week", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				int value = parseInt(params[2]);
+				if (value == Integer.MIN_VALUE) {
+					notNumber(event);
+					return false;
+				}
+				ldata.setMaxSetsPerWeek(value);
+				ldata.updateOptions(event.getGuild());
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("weeks-before-auto-inactive", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				int value = parseInt(params[2]);
+				if (value == Integer.MIN_VALUE) {
+					notNumber(event);
+					return true;
+				}
+				ldata.setWeeksBeforeAutoInactive(value);
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("weeks-before-set-expires", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				int value = parseInt(params[2]);
+				if (value == Integer.MIN_VALUE) {
+					notNumber(event);
+					return true;
+				}
+				ldata.setWeeksBeforeSetExpires(value);
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("weeks-before-set-repeat", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				int value = parseInt(params[2]);
+				if (value == Integer.MIN_VALUE) {
+					notNumber(event);
+					return true;
+				}
+				ldata.setWeeksBeforeSetRepeat(value);
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("default-score", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				int value = parseInt(params[2]);
+				if (value == Integer.MIN_VALUE) {
+					notNumber(event);
+					return true;
+				}
+				ldata.setDefaultScore(value);
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("K", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				double valueD = parseDouble(params[2]);
+				if (valueD < 0) {
+					notNumber(event);
+					return true;
+				}
+				ldata.setK(valueD);
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+valueD+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("auto-gen-pairs", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				boolean valueB;
+				if (params[2].equals("true")) valueB = true;
+				else if (params[2].equals("false")) valueB = false;
+				else {
+					event.getChannel().sendMessage(Important.getError()+" is not true or false!").queue();
+					return true;
+				}
+				ldata.autoGenPairs = valueB;
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+valueB+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+		subCommands.put("auto-update-ranks", new SubCommand() {
+			@Override
+			public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+				boolean valueB;
+				if (params[2].equals("true")) valueB = true;
+				else if (params[2].equals("false")) valueB = false;
+				else {
+					event.getChannel().sendMessage(Important.getError()+" is not true or false!").queue();
+					return true;
+				}
+				ldata.autoUpdateRanks = valueB;
+				event.getChannel().sendMessage("`"+params[1]+"` set to `"+valueB+"`").queue();
+				GlobalData.saveData();
+				return true;
+			}
+		});
+	}
 
 	@Override
 	public boolean runCommand(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
@@ -42,97 +158,7 @@ public class Config extends LeagueCommand {
 			event.getChannel().sendMessage(Important.getError()+" DO: "+getHelp()).queue();
 			return false;
 		}
-		int value;
-		double valueD;
-		boolean valueB = false;
-		switch (params[1]) {
-		case "max-sets-per-week":
-			value = parseInt(params[2]);
-			if (value == Integer.MIN_VALUE) {
-				notNumber(event);
-				return true;
-			}
-			ldata.setMaxSetsPerWeek(value);
-			ldata.updateOptions(event.getGuild());
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "weeks-before-auto-inactive":
-			value = parseInt(params[2]);
-			if (value == Integer.MIN_VALUE) {
-				notNumber(event);
-				return true;
-			}
-			ldata.setWeeksBeforeAutoInactive(value);
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "weeks-before-set-expires":
-			value = parseInt(params[2]);
-			if (value == Integer.MIN_VALUE) {
-				notNumber(event);
-				return true;
-			}
-			ldata.setWeeksBeforeSetExpires(value);
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "weeks-before-set-repeat":
-			value = parseInt(params[2]);
-			if (value == Integer.MIN_VALUE) {
-				notNumber(event);
-				return true;
-			}
-			ldata.setWeeksBeforeSetRepeat(value);
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "default-score":
-			value = parseInt(params[2]);
-			if (value == Integer.MIN_VALUE) {
-				notNumber(event);
-				return true;
-			}
-			ldata.setDefaultScore(value);
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+value+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "K":
-			valueD = parseDouble(params[2]);
-			if (valueD < 0) {
-				notNumber(event);
-				return true;
-			}
-			ldata.setK(valueD);
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+valueD+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "auto-gen-pairs":
-			if (params[2].equals("true")) valueB = true;
-			else if (params[2].equals("false")) valueB = false;
-			else {
-				event.getChannel().sendMessage(Important.getError()+" is not true or false!").queue();
-				return true;
-			}
-			ldata.autoGenPairs = valueB;
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+valueB+"`").queue();
-			GlobalData.saveData();
-			return true;
-		case "auto-update-ranks":
-			if (params[2].equals("true")) valueB = true;
-			else if (params[2].equals("false")) valueB = false;
-			else {
-				event.getChannel().sendMessage(Important.getError()+" is not true or false!").queue();
-				return true;
-			}
-			ldata.autoUpdateRanks = valueB;
-			event.getChannel().sendMessage("`"+params[1]+"` set to `"+valueB+"`").queue();
-			GlobalData.saveData();
-			return true;
-		}
-		event.getChannel().sendMessage(Important.getError()
-				+" setting "+params[1]+" doesn't exist. Try `"+BotMain.PREFIX+"help`!").queue();
-		return false;
+		return runSubCommands(event, params, gdata, ldata) == SubCommandResult.SUCCESS;
 	}
 	
 	private int parseInt(String param) {
--- src/main/java/com/onewho/gamerbot/command/ICommand.java
+++ src/main/java/com/onewho/gamerbot/command/ICommand.java
@@ -2,7 +2,9 @@ package com.onewho.gamerbot.command;
 
 import java.util.HashMap;
 
+import com.onewho.gamerbot.BotMain;
 import com.onewho.gamerbot.data.GuildData;
+import com.onewho.gamerbot.data.Important;
 import com.onewho.gamerbot.data.LeagueData;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -21,11 +23,24 @@ public interface ICommand {
 	
 	public HashMap<String, SubCommand> subCommands = new HashMap<>();
 	
-	default boolean runSubCommands(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
-		if (params.length < 2) return false;
+	default SubCommandResult runSubCommands(MessageReceivedEvent event, String[] params, GuildData gdata, LeagueData ldata) {
+		if (params.length < 2) return SubCommandResult.NO_PARAMS;
 		SubCommand sc = subCommands.get(params[1]);
-		if (sc == null) return false;
-		return sc.runCommand(event, params, gdata, ldata);
+		if (sc == null) {
+			event.getChannel().sendMessage(Important.getError()
+					+" setting "+params[1]+" doesn't exist. Try `"+BotMain.PREFIX+"help`!")
+				.queue();
+			return SubCommandResult.PARAM_DNE;
+		}
+		if (!sc.runCommand(event, params, gdata, ldata)) return SubCommandResult.COMMAND_FAIL;
+		return SubCommandResult.SUCCESS;
+	}
+	
+	public static enum SubCommandResult {
+		NO_PARAMS,
+		PARAM_DNE,
+		COMMAND_FAIL,
+		SUCCESS
 	}
 	
 }
